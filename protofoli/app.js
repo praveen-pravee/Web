8905b07aa0a17ff5cbd42e407bb33011428eb036
praveen <128697412+Thekid-pveen@users.noreply.github.com>
2023-08-27 16:56:19 +0530

    Update app.js
    Signed-off-by: praveen <128697412+Thekid-pveen@users.noreply.github.com>

--- protofoli/app.js
+++ protofoli/app.js
@@ -86,7 +86,7 @@ particlesJS('particles-js',
       "events": {
         "onhover": {
           "enable": true,
-          "mode": "grab"
+          "mode": "none"
         },
         "onclick": {
           "enable": true,
